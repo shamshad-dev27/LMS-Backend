c3209932ee33ccee41ab6541fa6009539f084932
shamshad-dev27 <shamshadahmad843127@gmail.com>
2026-04-21 15:51:51 +0530

    modle

--- models/user.modle.js
+++ models/user.modle.js
@@ -2,43 +2,43 @@ import { Schema, model } from "mongoose";
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import crypto from "crypto";
+
 const userSchema = new Schema({
     fullName: {
-        type: 'String',
-        required: [true, 'Name is require'],
-        minLenght: [5, 'Name is require of 5 character'],
-        maxLenght: [50, 'Name is under of 50 character'],
+        type: String,
+        required: [true, 'Name is required'],
+        minLength: [5, 'Name must be at least 5 characters'],
+        maxLength: [50, 'Name must be under 50 characters'],
         lowercase: true,
         trim: true,
     },
     email: {
-        type: 'String',
-        required: [true, 'email is require'],
+        type: String,
+        required: [true, 'Email is required'],
         lowercase: true,
         trim: true,
         unique: true,
         match: [
             /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-            " Please enter a valid email address"
+            "Please enter a valid email address"
         ],
     },
     password: {
-        type: 'String',
-        required: [true, 'password is require'],
-        minLenght: [8, 'password must be 8 character'],
+        type: String,
+        required: [true, 'Password is required'],
+        minLength: [8, 'Password must be at least 8 characters'],
         select: false,
-    }
-    ,
+    },
     avatar: {
         public_id: {
-            type: 'String'
+            type: String
         },
         secure_url: {
-            type: 'String'
+            type: String
         }
     },
     role: {
-        type: 'String',
+        type: String,
         enum: ['User', 'ADMIN'],
         default: 'User'
     },
@@ -56,19 +56,18 @@ userSchema.methods = {
     generateJWTtoken: async function () {
         return await jwt.sign(
             {
-                id: this._id, email: this.email, subscription: this.subscription, role: this.role
+                id: this._id, email: this.email,
+                subscription: this.subscription, role: this.role
             },
             process.env.JWT_SECRET,
-            {
-                expiresIn: process.env.JWT_EXPIRE
-            }
+            { expiresIn: process.env.JWT_EXPIRE }
         )
     },
     comparePassword: async function (plaintextPassword) {
         return await bcrypt.compare(plaintextPassword, this.password)
     },
     generatePasswordResetToken: async function () {
-        const resetToken = await crypto.randomBytes(20).toString('hex');
+        const resetToken = crypto.randomBytes(20).toString('hex');
         this.forgotPasswordToken = crypto
             .createHash('sha256')
             .update(resetToken)
@@ -76,18 +75,13 @@ userSchema.methods = {
         this.forgotPasswordExpire = Date.now() + 15 * 60 * 1000;
         return resetToken;
     }
-
 }
 
 userSchema.pre('save', async function () {
-    if (!this.isModified('password')) {
-        return;
-    }
+    if (!this.isModified('password')) return;
     this.password = await bcrypt.hash(this.password, 10);
-
-})
+});
 
 const User = model('Users', userSchema);
-
 
 export default User;
